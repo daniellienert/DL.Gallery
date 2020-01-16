1cfb054844f923be320ee5471859c63110b9802e
Christian Keuerleber <keuerleber@punkt.de>
2020-01-16 15:48:10 +0100

    TASK: fix watcher task in gulp

--- Resources/Private/gulpfile.js
+++ Resources/Private/gulpfile.js
@@ -53,15 +53,15 @@ const javascript = (subTaskName) => {
         .pipe(uglify())
         .pipe(concat(filesAndDirectories[subTaskName].destination.fileName))
         .pipe(gulp.dest(filesAndDirectories[subTaskName].destination.path))
-}
+};
 
 const bootstrapLightbox = () => {
     return javascript('bootstrapLightbox')
-}
+};
 
 const justifiedGallery = () => {
     return javascript('justifiedGallery')
-}
+};
 
 const styles = () => {
     return gulp
@@ -70,13 +70,13 @@ const styles = () => {
         .pipe(sassGlob())
         .pipe(sass())
         .pipe(gulp.dest(filesAndDirectories.styles.destination));
-}
+};
 
 const registerWatchers = () => {
     gulp.watch(filesAndDirectories.bootstrapLightbox.source, bootstrapLightbox);
     gulp.watch(filesAndDirectories.justifiedGallery.source, justifiedGallery);
 
-    gulp.watch(filesAndDirectories.sass.src, sass);
+    gulp.watch(filesAndDirectories.styles.source, sass);
 };
 
 const allJavascript = gulp.parallel(bootstrapLightbox, justifiedGallery);
